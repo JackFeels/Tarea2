42dbd805ec0cec8d8e8c2414d9480bf1bd241907
JackFeels <107821314+JackFeels@users.noreply.github.com>
2022-10-18 18:49:57 -0300

    Creacion de Exceptions

--- src/tarea2/NoHayBebidaException.java
+++ src/tarea2/NoHayBebidaException.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package tarea2;
 
-/**
- *
- * @author jackfeels
- */
-public class NoHayBebidaException {
+public class NoHayBebidaException extends Exception {
+    
     
 }
