7b7d0396753ccb6b3bc45c75c2a2fd4709de6af2
matiasmdlp <matiasmdlp95@gmail.com>
2022-10-18 17:25:36 -0300

    Comprador

--- src/tarea2/Comprador.java
+++ src/tarea2/Comprador.java
@@ -5,13 +5,14 @@ public class Comprador {
     private int vuelto;
     private String queBebida;
     public Comprador(Moneda m, int cualBebida, Expendedor exp){
+        this.vuelto = 0;
         Bebida b = exp.comprarBebida(m, cualBebida);
         if(b != null){
             this.queBebida = b.beber();
         }
-        Moneda mon = null;
-        while((mon = exp.getVuelto()) != null){
-            this.vuelto = vuelto + mon.getValor();
+        
+        while(exp.getVuelto() != null){
+            this.vuelto = this.vuelto + exp.getVuelto().getValor();
         }        
     }
     public int getVuelto(){
